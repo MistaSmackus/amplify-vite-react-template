78d507d7703785a7409ac8a7063f988b6aa5dad4
MistaSmackus <mistacalebothieno@gmail.com>
2025-03-26 21:10:41 -0500

    bootstrapcss
    import 'bootstrap/dist/css/bootstrap.min.css'

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,62 @@
-import { useEffect, useState } from "react";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { BrowserRouter as Router, Route, Routes, NavLink} from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import './App.css';
+{/*import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import { generateClient } from "aws-amplify/data";*/}
+import Home from './Home.tsx';
+import Portfolio from "./Portfolio.tsx";
+import Market from './Market.tsx';
+import UserTransaction from './UserTransaction.tsx';
+import Admin from './Admin.tsx';
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  const {user, signOut} = useAuthenticator();
+  
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+        <Router>
+          <Nav variant="tabs" className="bg-body-tertiary">
+            <Nav.Item>
+              <NavLink to="/" className="nav-link">Titan Equity Group</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/portfolio" className="nav-link">Your Portfolio</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/market" className="nav-link">Market</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/userTransaction" className="nav-link">Transactions</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+              <NavLink to="/admin" className="nav-link">Admin</NavLink>
+            </Nav.Item> 
+            <Nav.Item>
+              <Button onClick={signOut} id="signOutButton">Sign out</Button>
+            </Nav.Item>
+          </Nav>
+          <Navbar.Text className="justify-content-end">Signed In as: {user?.signInDetails?.loginId}</Navbar.Text>
+          
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />}/>
+            <Route path="/market" element={<Market />}/>
+            <Route path="/userTransaction" element={<UserTransaction />}/>
+            <Route path="/admin" element={<Admin />}/> 
+          </Routes>
+
+        </Router>
+
+      {/* Footer */}
+      <footer className="mt-4 text-muted">
+        <p>&copy; {new Date().getFullYear()} Titan Equity Group. All rights reserved.</p>
+      </footer>
     </main>
   );
 }
