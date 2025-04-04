0d70818d7476bcf26e18ad3b861cb1662c9f5630
MistaSmackus <mistacalebothieno@gmail.com>
2025-04-04 12:03:23 -0500

    test
    xD

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "teggapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "teggapp691e1703": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
